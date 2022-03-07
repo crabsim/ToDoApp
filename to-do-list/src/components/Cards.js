f41d752b61495e3531d63b02962d7c9dabe7e6af
Simran_Tekion <crabsim1@gmail.com>
2022-03-07 13:17:25 +0530

    cards Data filter

--- to-do-list/src/components/Cards.js
+++ to-do-list/src/components/Cards.js
@@ -7,7 +7,7 @@ function Cards(props) {
             return(
                 <div className='column align-base default-color category'>
                     <h2>{category}</h2>
-                    {props.cardsData.filter((card)=>card.category==category).map((cardData,index)=>{
+                    {props.cardsData.filter( card => card.category === category).map((cardData,index)=>{
                         return <Card key={`${categoryIndex}-${index}`} data={cardData} deleteCard={props.deleteCard} openModalUpdate={props.openModalUpdate}  />
                     })}
                  </div>   
