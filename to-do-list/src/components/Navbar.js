193556f41314b401bbc49f597dec0aa28d6e3eff
Simran_Tekion <crabsim1@gmail.com>
2022-03-07 12:08:45 +0530

    first commit

---
+++ to-do-list/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './Navbar.css';
+
+class Navbar extends Component{
+    render(){
+        return(
+            <div className="default-color navbar">
+                <h2>Todo List Manager</h2>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
